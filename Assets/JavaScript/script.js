66a3a3d2d6fab3c3065b4bb8c1b07dab3aa7dece
“Matthew <mpick90sick@gmail.com>
2023-08-16 16:21:39 -0400

    Green texted code and edited the script.js link

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,7 +2,7 @@ var startScreen = document.querySelector(".startScreen")
 var questionScreen = document.querySelector(".questionScreen")
 var initialSumbit = document.querySelector(".initialSubmit")
 var startBtn = document.querySelector(".startBtn")
-var questionText = document.querySelector(".questionText")
+var questionText = document.querySelector("#questionBox")
 var answerText = document.querySelector(".answerText")
 var initialValue = document.querySelector(".initialValue")
 var submitBtn = document.querySelector(".submitBtn")
@@ -17,20 +17,35 @@ var questionArray = [
         question: "What is 1+5?", 
         choices: [6, 5, 7, "Spaghetti"],
         correct: 6
+    },
+    {
+        question: "What is the meaning of life?",
+        choices: [6, 5, 7, "Spaghetti"],
+        correct: "Spaghetti"
     }
 ]
-function startTimer() {
 
-}
+
+
+//function questionScreen()
+//function startTimer() {
+
+//}
+
 function displayQuestions() {
-questionText.textContent= questionArray [qIndex].question
-questionArray [qIndex].choices; 
-for(let qIndex = 0; qIndex < 5; qIndex++) {
-console.log("nextQuestion")
-}}
+    console.log('func-called-display')
+    startScreen.classList.add('hide');
+    questionScreen.classList.remove('hide');
+questionText.textContent= questionArray[qIndex].question
+    // questionArray[qIndex].choices; 
+    // for(let qIndex = 0; qIndex < 5; qIndex++) {
+    // console.log("nextQuestion")
+    // }
+    }
+
 
 startBtn.addEventListener("click", function() {
-    startTimer()
+    //startTimer()
     displayQuestions()
     
 })
